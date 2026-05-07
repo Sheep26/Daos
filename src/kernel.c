7efb4d1c434fa37d28d0be9a0ed46c6376a3070d
Sheep26 <crabeykiller@gmail.com>
2026-05-07 12:10:55 +1200

    comments

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,15 @@
 #include "io.h"
 #include "vga.h"
 
-char * itoa(int value, char* str, int base) {
-    char * rc;
-    char * ptr;
-    char * low;
+char* itoa(int value, char* str, int base) {
+    /* 
+    @param int value - The value of the int.
+    @param char* str - The buffer for the output.
+    @param int base - The base of the int (ie normal numbers would be base 10)
+    */
+    char* rc;
+    char* ptr;
+    char* low;
     // Check for supported base.
     if ( base < 2 || base > 36 )
     {
